8e36c37eb59e93ef92f669c502548f0ca5f2109b
Abdylo1488 <grisanet845@gmail.com>
2026-04-10 00:07:28 +0300

    Update shooter_game.py
    игрулька

--- ИГРА ПУДЖАП/shooter_game.py
+++ ИГРА ПУДЖАП/shooter_game.py
@@ -2,24 +2,19 @@ import os
 from pygame import *
 from random import randint, choice
 
-# Настройка папки
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# --- Инициализация ---
 init()
 mixer.init()
 font.init()
 
-# Получаем разрешение монитора, чтобы игра подстроилась под любой экран
 info = display.Info()
 win_width = info.current_w
 win_height = info.current_h
 
-# Создание окна во весь экран
 window = display.set_mode((win_width, win_height), FULLSCREEN)
-display.set_caption("Shooter: Final Boss Fullscreen")
+display.set_caption("Shooter")
 
-# --- Настройки ---
 FPS = 60
 score = 0
 lost = 0
@@ -27,17 +22,15 @@ is_boss_alive = False
 finish = False
 game = True
 
-font_main = font.Font(None, 36)
-font_win = font.Font(None, 80)
+font_main = font.Font(None, 45)
+font_win = font.Font(None, 100)
 
-# --- Ресурсы ---
-# Теперь картинки масштабируются под размер вашего экрана (win_width, win_height)
 img_back = "пуджара.jpg"
 img_hero = "птица.png"
 img_bullet = "images-_6_.png"
-img_scrimer = "scrimer.png"    
-img_boss = "ivan1.png"         
-enemy_images = ['ivan2.png', '8.jpg', 'ьувмув.png', 'пнг11.png']
+img_scrimer = "scrimer.jpg"    
+img_boss = "ьувмув.png"         
+enemy_images = ['ivan2.png', '8.jpg', 'ivan1.png', 'пнг11.png']
 
 try:
     mixer.music.load('08279.wav')
@@ -45,11 +38,10 @@ try:
     mixer.music.set_volume(0.3)
     scream_sound = mixer.Sound('scream.wav')
 except:
-    print("Звуковые файлы не найдены")
+    pass
 
 background = transform.scale(image.load(img_back), (win_width, win_height))
 
-# --- Классы ---
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, w, h):
         super().__init__()
@@ -70,13 +62,12 @@ class Player(GameSprite):
     def update(self):
         keys = key.get_pressed()
         if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= 10
-        if keys[K_RIGHT] and self.rect.x < win_width - 70:
-            self.rect.x += 10
+            self.rect.x -= 15
+        if keys[K_RIGHT] and self.rect.x < win_width - self.rect.width:
+            self.rect.x += 15
             
     def fire(self):
-        # Пули подстроены под масштаб
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, 30)
+        bullet = Bullet(img_bullet, self.rect.centerx - 10, self.rect.top, 15, 30, 45)
         bullets.add(bullet)
 
 class Enemy(GameSprite):
@@ -84,8 +75,8 @@ class Enemy(GameSprite):
         self.rect.y += self.speed
         global lost
         if self.rect.y > win_height:
-            self.rect.x = randint(50, win_width - 80)
-            self.rect.y = -40
+            self.rect.x = randint(50, win_width - 100)
+            self.rect.y = -100
             lost += 1
 
 class Boss(GameSprite):
@@ -100,7 +91,7 @@ class Boss(GameSprite):
         self.rect.y += self.speed_y
         if self.rect.x <= 0 or self.rect.x >= win_width - self.rect.width:
             self.speed_x *= -1
-        if self.rect.y <= 0 or self.rect.y >= win_height // 2:
+        if self.rect.y <= 0 or self.rect.y >= win_height - self.rect.height:
             self.speed_y *= -1
 
 class Bullet(GameSprite):
@@ -109,20 +100,16 @@ class Bullet(GameSprite):
         if self.rect.y < 0:
             self.kill()
 
-# --- Объекты ---
-# Корабль внизу экрана независимо от разрешения
-ship = Player(img_hero, win_width//2, win_height - 100, 10, 80, 80)
+ship = Player(img_hero, win_width//2, win_height - 150, 15, 100, 100)
 bullets = sprite.Group()
 enemies = sprite.Group()
 
 def create_enemies():
     for i in range(5):
-        enemy = Enemy(choice(enemy_images), randint(50, win_width - 80), -40, randint(2, 4), 60, 60)
+        enemy = Enemy(choice(enemy_images), randint(50, win_width - 100), -100, randint(1, 2), 90, 90)
         enemies.add(enemy)
 
 create_enemies()
-
-# --- Игровой цикл ---
 clock = time.Clock()
 won = False
 
@@ -133,15 +120,13 @@ while game:
         elif e.type == KEYDOWN:
             if e.key == K_SPACE and not finish:
                 ship.fire()
-            if e.key == K_ESCAPE: # Выход по нажатию Esc
+            if e.key == K_ESCAPE:
                 game = False
 
     if not finish:
         window.blit(background, (0, 0))
-        
-        # Статистика
-        window.blit(font_main.render("Счет: " + str(score), True, (255, 255, 255)), (20, 20))
-        window.blit(font_main.render("Пропущено: " + str(lost), True, (255, 255, 255)), (20, 60))
+        window.blit(font_main.render("Убито: " + str(score), True, (255, 255, 255)), (30, 30))
+        window.blit(font_main.render("Пропущено: " + str(lost), True, (255, 255, 255)), (30, 80))
 
         ship.update()
         enemies.update()
@@ -151,21 +136,20 @@ while game:
         enemies.draw(window)
         bullets.draw(window)
 
-        # Логика столкновений
         if not is_boss_alive:
             collides = sprite.groupcollide(enemies, bullets, True, True)
             for c in collides:
                 score += 1
-                new_enemy = Enemy(choice(enemy_images), randint(50, win_width - 80), -40, randint(2, 4), 60, 60)
+                new_enemy = Enemy(choice(enemy_images), randint(50, win_width - 100), -100, randint(1, 2), 90, 90)
                 enemies.add(new_enemy)
             
-            if score >= 50:
+            if score >= 2:
                 is_boss_alive = True
                 for e in enemies: e.kill()
-                boss = Boss(img_boss, win_width//2, 50, 5, 250, 250, 20)
+                boss = Boss(img_boss, win_width//2, 100, 6, 300, 300, 25)
                 enemies.add(boss)
         else:
-            window.blit(font_main.render("HP БОССА: " + str(boss.hp), True, (255, 0, 0)), (win_width//2 - 50, 20))
+            window.blit(font_main.render("HP БОССА: " + str(boss.hp), True, (255, 0, 0)), (win_width//2 - 100, 30))
             boss_hit = sprite.spritecollide(boss, bullets, True)
             if boss_hit:
                 boss.hp -= 1
@@ -174,33 +158,31 @@ while game:
                     finish = True
                     won = True
 
-        # Проверка поражения
-        if sprite.spritecollide(ship, enemies, False) or lost >= 10:
+        if sprite.spritecollide(ship, enemies, False) or lost >= 100:
             if is_boss_alive:
                 mixer.music.stop()
                 try:
                     scream_sound.play()
-                    scrimer_img = transform.scale(image.load(img_scrimer), (win_width, win_height))
-                    window.blit(scrimer_img, (0, 0))
-                    display.update()
-                    time.delay(2000) 
-                except: pass
+                    scr_img = transform.scale(image.load(img_scrimer), (win_width, win_height))
+                    window.blit(scr_img, (0, 0))
+                    display.flip() # Обновляем экран немедленно
+                    time.wait(2000) # Замираем на 2 секунды
+                except:
+                    pass
             finish = True
             won = False
 
         display.update()
     else:
-        # Финальный экран
         if won:
             msg = font_win.render("ПОБЕДА!", True, (0, 255, 0))
         else:
-            msg = font_win.render("ИГРА ОКОНЧЕНА", True, (255, 0, 0))
+            msg = font_win.render("ТЫ ПРОИГРАЛ", True, (255, 0, 0))
         
-        window.blit(msg, (win_width//2 - 200, win_height//2))
+        window.blit(msg, (win_width//2 - 250, win_height//2))
         display.update()
         time.delay(3000)
         
-        # Сброс
         score, lost = 0, 0
         is_boss_alive, finish, won = False, False, False
         for e in enemies: e.kill()
